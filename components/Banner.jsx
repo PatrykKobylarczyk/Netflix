738ed3c2586a8ab2b72d6d66747f87b2bf745076
Patryk Kobylarczyk <patryk.kobylarczyk@gmail.com>
2022-11-15 12:06:32 +0100

    add title and description on Banner

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { baseUrl } from "../constans/movie";
+import { FaPlay } from 'react-icons/fa';
+import { InformationCircleIcon } from "@heroicons/react/solid";
 
 const Banner = ({ netflixOriginals }) => {
   const [movie, setMovie] = useState(null);
@@ -16,7 +18,7 @@ const Banner = ({ netflixOriginals }) => {
   }, [movie]);
 
   return (
-    <div>
+    <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:pb-12">
       <div className="absolute top-0 left-0 h-[95vh] w-screen -z-10">
         <Image
           src={`${baseUrl}${
@@ -28,7 +30,15 @@ const Banner = ({ netflixOriginals }) => {
         />
       </div>
 
-      <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+      <h1 className="text-2xl md:text-4xl lg:text-7xl font-bold">
+        {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+      <p className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-w-2xl">{movie?.overview}</p>
+
+      <div className="flex space-x-3">
+          <button className="bannerButton bg-white text-black"><FaPlay className="h-4 w-4 text-black md:h-7 md:w-7"/>Play</button>
+          <button className="bannerButton bg-[gray]/70">More Info <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8"/></button>
+      </div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,9 @@ import Banner from "../components/Banner";
 
 //OTHERS
 import requests from "../utils/requests";
-import { useEffect } from "react";
 
 
 const Home = ({ netflixOriginals,}) => {
-
-    console.log(netflixOriginals)
-
-
-
 
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
@@ -26,8 +20,7 @@ const Home = ({ netflixOriginals,}) => {
       {/* header */}
 
       <Header />
-      <main className="w-full h-screen grid place-items-center">
-        {/* Banner */}
+      <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
         <Banner netflixOriginals={netflixOriginals}/>
 
         <section>
