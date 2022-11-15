b9c92b01f683795b140d27dbd3636ad1c6546a5e
Patryk Kobylarczyk <patryk.kobylarczyk@gmail.com>
2022-11-15 15:50:19 +0100

    add movies/thumbnails section

---
+++ components/Thumbnail.jsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+const Thumbnail = ({ movie }) => {
+  return (
+    <div className="relative h-28 md:h-36 lg:h-40  min-w-[220px] cursor-pointer transition duration-200 ease-out md:min-w-[300px] md:hover:scale-105">
+      <Image
+        src={`https://image.tmdb.org/t/p/w500${
+          movie.backdrop_path || movie.poster_path
+        }`}
+        className="rounded-sm object-cover md:rounded"
+        layout="fill"
+      />
+    </div>
+  );
+};
+
+export default Thumbnail;
