5868967909a7e2868762e47a5ce59a5b2bcb2ce7
Patryk Kobylarczyk <patryk.kobylarczyk@gmail.com>
2022-11-27 13:47:24 +0100

    add full account page

--- pages/account.jsx
+++ pages/account.jsx
@@ -52,7 +52,7 @@ const Account = ({ products }) => {
         <div className="mt-6 grid grid-cols-1 gap-x-4 border px-4 py-4 md:grid-cols-4 md:border-x-0 md:border-t md:border-b-0 md:px-0 md:pb-0">
           <h4>Plan Details</h4>
           {/* Find the current plan */}
-          <div className="">
+          <div className="col-span-2 font-medium">
             {
               products.filter(
                 (product) => product.id === subscription?.product
